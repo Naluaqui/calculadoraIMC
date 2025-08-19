5325a4764f0b7789752317ec39cad5e2b28eea00
Ana Luiza <analu.tibi@outlook.com>
2025-08-19 11:20:13 -0300

    Arrumando versões de branchs

--- calculadoraIMC/src/App.jsx
+++ calculadoraIMC/src/App.jsx
@@ -1,8 +1,17 @@
+import Header from "./components/Header"
+
 function App(){
   return (
-    <Header/>
-    <>
-    </>
+
+    <div className="container">
+        <div className="box">
+          <Header/>
+          <form>
+
+          </form>
+        </div>
+    </div>
+    
   )
 }
 export default App
