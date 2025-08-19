97de43fed93951a08b83864ca03d4c01096bcbba
Ana Luiza <analu.tibi@outlook.com>
2025-08-19 11:34:23 -0300

    1. Iniciando estrutura

--- calculadoraIMC/src/App.jsx
+++ calculadoraIMC/src/App.jsx
@@ -1,17 +1,39 @@
 import Header from "./components/Header"
+import Resultado from "./components/Resultado"
+import { useEffect,useState } from "react"
 
 function App(){
+
+  const [peso,setPeso] = useState(0);
+  const [altura,setAltura] = useState(0);
+  const [resultado,setResultado] = useState(0);
+
   return (
 
     <div className="container">
         <div className="box">
           <Header/>
           <form>
+            <div>
+              <label htmlFor="altura">Altura <span>(exemplo:1.8)</span></label>
+              <input id="altura" type="number" placeholder="Digite sua altura" onBlur={({target})=>parseInt(setAltura(target.value))}/>
+            </div>
+
+            <div>
+              <label htmlFor="peso">Peso <span>(exemplo:80)</span></label>
+              <input id="peso" type="number" placeholder="Digite seu peso" onBlur={({target})=>parseInt(setPeso(target.value))}/>
+            </div>
+
+            <button onClick={calculadorImc}>Calcular</button>
 
           </form>
         </div>
+
+        {mostrarResultado && (
+          <Resultado resultado={resultado}/>
+        )}
     </div>
-    
   )
 }
+
 export default App
